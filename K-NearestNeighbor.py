f8a28c4c022671e49e333da9897721f5b8aa4c68
Pepper-13 <pepper_1317@yahoo.com>
2017-09-14 00:44:13 +0530

    Update K-NearestNeighbor.py

--- K-NearestNeighbor.py
+++ K-NearestNeighbor.py
@@ -3,3 +3,17 @@
 import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
+
+df = pd.read_csv("C:/Users/Hi/Documents/Prep/Datasets/breast-cancer-wisconsin.data.txt")
+df.replace("?", -99999, inplace = True)
+df.drop(['Id'], 1, inplace = True)
+df.drop(['Unnamed: 11'], 1, inplace = True)
+
+X = np.array(df.drop['class'], 1)
+y = np.array(df['class'])
+
+X_train, y_train, X_test, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
