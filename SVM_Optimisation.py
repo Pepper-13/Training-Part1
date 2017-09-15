bf230683b9064e009e7af4dbf5fb2a43789c299b
Pepper-13 <pepper_1317@yahoo.com>
2017-09-15 15:59:30 +0530

    Update SVM_Optimisation.py

--- SVM_Optimisation.py
+++ SVM_Optimisation.py
@@ -22,3 +22,14 @@ class Support_Vector_Machine:
                      [1,-1],
                      [-1,1],
                      [-1,-1]]
+        
+        all_data = []
+        for yi in self.data:
+            for featureset  in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+        
